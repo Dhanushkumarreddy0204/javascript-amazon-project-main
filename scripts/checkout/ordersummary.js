75d4a97c545fdd20fed33fed365555e42aaafbfb
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-01-01 18:00:51 +0530

    importing external librery to day,and dates in the the js projects

--- scripts/checkout/ordersummary.js
+++ scripts/checkout/ordersummary.js
@@ -1,3 +1,4 @@
 import { cart, removefromcart, updateQuantity, updateDeliveryOption } from "../../data/cart.js";
 import { products } from "../../data/products.js";
 import { formatcurrency } from "../utils/money.js";
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
