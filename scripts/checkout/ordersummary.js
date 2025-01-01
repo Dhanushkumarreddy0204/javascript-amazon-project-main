daa5f99431764de1134f46cda3b9aac2346f09a3
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-01-01 17:59:57 +0530

    imprting money formating method to order to show in dollers

--- scripts/checkout/ordersummary.js
+++ scripts/checkout/ordersummary.js
@@ -1,2 +1,3 @@
 import { cart, removefromcart, updateQuantity, updateDeliveryOption } from "../../data/cart.js";
 import { products } from "../../data/products.js";
+import { formatcurrency } from "../utils/money.js";
