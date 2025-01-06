353a98e14cc18f07f14723946ba705499e1ff75c
C. DHANUSH KUMAR REDDY <156655062+Dhanushkumarreddy0204@users.noreply.github.com>
2025-01-06 23:45:00 +0530

    giving product rating by the data in the products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -14,5 +14,13 @@ products.forEach((product) => {
            <div class="product-name limit-text-to-2-lines">
             ${product.name}
           </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars*10}.png">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
           `;
         });
